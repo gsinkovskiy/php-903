03ab0b31d0e0b2ecdddc84e4b4c47f2a7069eb29
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-05-29 20:17:40 +0300

    lesson 3

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Форма</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<h1>Получено:</h1>
+<table>
+<?php foreach ($_REQUEST as $field => $value): ?>
+	<tr>
+		<th><?= $field ?></th>
+		<td><?= htmlentities($value) ?></td>
+	</tr>
+<?php endforeach ?>
+</table>
+
